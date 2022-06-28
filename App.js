1974e0a47c1aa57a674dc48ca565f1dc3c2c5bd1
unknown <LHP6.jduparc@gmail.com>
2022-06-28 09:53:44 +0200

    link firebase ok

---
+++ App.js
@@ -0,0 +1,29 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+const App= () => {
+  return (
+    <View>
+      <Text>Hello World ! julien is the best </Text>
+    </View>
+  )
+}
+
+
+export default App;
